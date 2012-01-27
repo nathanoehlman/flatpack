7b1c346440ab205079107d3ff25356f488153095
Nathan Oehlman <nathan.oehlman@sidelab.com>
2012-01-27 15:32:08 +1000

    Formatting fixes

--- flatpack.js
+++ flatpack.js
@@ -1,7 +1,7 @@
 var models = {},
     supercomfy = require('supercomfy'),
-	config = require('config'),
-	views = require('./lib/views');
+    config = require('config'),
+    views = require('./lib/views');
 
 /**
   Defines a model, checking with the CouchDB database
@@ -11,17 +11,15 @@ var models = {},
  **/
 exports.define = function(db, model, options, callback) {
 
-	function createView(db, attributes, callback) {
-		
-	}
-	
+    
+    
 }
 
 /**
   Returns a data accessor for the given model
  **/
 exports.use = function(model) {
-	
+    
 }
 
 /**
@@ -29,39 +27,39 @@ exports.use = function(model) {
   the helpers for the models
  **/
 function DataAccessor(model, db) {
-	this.def = model;
-	this.db = db;
+    this.def = model;
+    this.db = db;
 }
 
 DataAccessor.prototype = {
 
-	/**
-	  Saves an instance of the object
-	 **/
-	save: function(object, callback) {
-		
-	},
-	
-	/**
-	  Deletes an instance of the object
-	 **/
-	delete: function(object, callback) {
-		
-	},
-	
-	/**
-	  Gets the instance of the object denoted by id, or null
-	 **/
-	get: function(id, callback) {
-		
-	},
-	
-	/**
-	  Returns an instance of the db for this helper
-	 **/
-	_db: function() {
-		return supercomfy(config.couchurl).use(this.db);
-	}
-	
+    /**
+      Saves an instance of the object
+     **/
+    save: function(object, callback) {
+        
+    },
+    
+    /**
+      Deletes an instance of the object
+     **/
+    delete: function(object, callback) {
+        
+    },
+    
+    /**
+      Gets the instance of the object denoted by id, or null
+     **/
+    get: function(id, callback) {
+        
+    },
+    
+    /**
+      Returns an instance of the db for this helper
+     **/
+    _db: function() {
+        return supercomfy(config.couchurl).use(this.db);
+    }
+    
 };
 
