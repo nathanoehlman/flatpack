ddac2187d208e8577888213ac4d0432d6d010641
Nathan Oehlman <nathan.oehlman@sidelab.com>
2012-01-30 10:24:37 +1000

    Added object retrieval by .get()

--- flatpack.js
+++ flatpack.js
@@ -1,10 +1,10 @@
 var models = {},
     supercomfy = require('supercomfy'),
-	config = require('config'),
-	views = require('./lib/views')
-	async = require('async'),
-	_ = require('underscore'),
-	debug = require('debug')('flatpack');
+    config = require('config'),
+    views = require('./lib/views')
+    async = require('async'),
+    _ = require('underscore'),
+    debug = require('debug')('flatpack');
 
 /**
   Defines a model, checking with the CouchDB database
@@ -14,57 +14,57 @@ var models = {},
  **/
 exports.define = function(couchurl, db, model, options, callback) {
 
-	if (!model || model.length == 0) return callback("Error: Invalid model name");
-	
-	// Get the views
-	var modelViews = {byType: []},
-		accessor = new Accessorizer(model, couchurl, db);
-		
-	_.extend(modelViews, (options && options.views) ? options.views : {});
-	
-	// Create a view
-	function createView(viewName, attributes, callback) {
-		views.createIfNotExists(couchurl, db, model, attributes, null, function(err, designName, dbView) {
-			if (err) return callback(err)
-			
-			if (attributes && attributes.length > 0) {
-				// Create the accessor, with args as params
-			} else {
-				// Create the accessor
-				accessor[viewName] = function(id, done) {
-					var db = this._getDB();
-					db.get(designName + '/view/' + dbView, {key:id}, done);
-				};
-				
-			}
-			return callback();
-		});
-	}
-	
-	// Create all views
-	async.forEach(_.keys(modelViews), 
-				  function(key, callback) {
-					 createView(key, modelViews[key], callback)
-				  },
-				  function(err) {
-					 if (err) {
-						// TODO: Maybe get rid of the model definition
-						return callback(err);
-					 }
-		             models[model] = accessor;
-					 return callback(null, accessor);
-	              });
+    if (!model || model.length == 0) return callback("Error: Invalid model name");
+    
+    // Get the views
+    var modelViews = {byType: []},
+        accessor = new Accessorizer(model, couchurl, db);
+        
+    _.extend(modelViews, (options && options.views) ? options.views : {});
+    
+    // Create a view
+    function createView(viewName, attributes, callback) {
+        views.createIfNotExists(couchurl, db, model, attributes, null, function(err, designName, dbView) {
+            if (err) return callback(err)
+            
+            if (attributes && attributes.length > 0) {
+                // Create the accessor, with args as params
+            } else {
+                // Create the accessor
+                accessor[viewName] = function(id, done) {
+                    var db = this._getDB();
+                    db.get(designName + '/view/' + dbView, {key:id}, done);
+                };
+                
+            }
+            return callback();
+        });
+    }
+    
+    // Create all views
+    async.forEach(_.keys(modelViews), 
+                  function(key, callback) {
+                     createView(key, modelViews[key], callback)
+                  },
+                  function(err) {
+                     if (err) {
+                        // TODO: Maybe get rid of the model definition
+                        return callback(err);
+                     }
+                     models[model] = accessor;
+                     return callback(null, accessor);
+                  });
 }
 
 /**
   Returns a data accessor for the given model
  **/
 exports.use = function(model) {
-	if (models && models[model]) {
-		return models[model];
-	}
-	debug('Model [' + model + '] not defined. Define with flatpack.define() first');
-	return null;
+    if (models && models[model]) {
+        return models[model];
+    }
+    debug('Model [' + model + '] not defined. Define with flatpack.define() first');
+    return null;
 }
 
 /**
@@ -72,74 +72,84 @@ exports.use = function(model) {
   the helpers for the models
  **/
 function Accessorizer(model, server, db) {
-	this.def = model;
-	this.server = server;
-	this.db = db;
+    this.def = model;
+    this.server = server;
+    this.db = db;
 }
 
 Accessorizer.prototype = {
 
-	/**
-	  Saves an instance of the object
-	 **/
-	save: function(object, callback) {
-		// Get values of the object
-		var db = this._getDB(),
-			data = {type: this.def},
-			id = (object && object._id) ? object._id : null;
-		_.extend(data, object);
-		
-		// Save the document
-		function saveDocument(data, callback) {
-			console.log(id);
-			console.log(data);
-			console.log(callback);
-			// Create a document for the client in the clients database
-			db.put(id, data, callback);
-		}
-		
-		// Check if this is an update
-		if (!id) {
-			// Get the UUID for the document
-			this._getUUID(function(err, uuid) {
-				if (err) return callback(err);
-				id = data['_id'] = uuid.uuids[0];
-				saveDocument(data, callback);
-			});	
-		} else {
-			saveDocument(data, callback);
-		}
-	
-	},
-	
-	/**
-	  Deletes an instance of the object
-	 **/
-	delete: function(object, callback) {
-		
-	},
-	
-	/**
-	  Gets the instance of the object denoted by id, or null
-	 **/
-	get: function(id, callback) {
-		
-	},
-	
-	/**
-	  Returns an instance of the db for this helper
-	 **/
-	_getDB: function() {
-		return supercomfy(this.server).use(this.db);
-	},
-	
-	/**
-	  Returns a generated UUID from couch to use as the id
-	 **/
-	_getUUID: function(callback) {
-		var db = supercomfy(config.couchurl);;		
-		db.get('_uuids', null, callback);
-	}
-	
+    /**
+      Saves an instance of the object, will return the saved objects id
+      as the second parameter of the callback. ie. callback(err, id);
+     **/
+    save: function(object, callback) {
+        // Get values of the object
+        var db = this._getDB(),
+            data = {type: this.def},
+            id = (object && object._id) ? object._id : null;
+        _.extend(data, object);
+        
+        // Save the document
+        function saveDocument(data, callback) {
+            console.log(id);
+            console.log(data);
+            console.log(callback);
+            // Create a document for the client in the clients database
+            db.put(id, data, function(err) {
+                if (err) return callback(err);
+                return callback(err, id);
+            });
+        }
+        
+        // Check if this is an update
+        if (!id) {
+            // Get the UUID for the document
+            this._getUUID(function(err, uuid) {
+                if (err) return callback(err);
+                id = data['_id'] = uuid.uuids[0];
+                saveDocument(data, callback);
+            }); 
+        } else {
+            saveDocument(data, callback);
+        }
+    
+    },
+    
+    /**
+      Deletes an instance of the object
+     **/
+    delete: function(object, callback) {
+        
+    },
+    
+    /**
+      Gets the instance of the object denoted by id, or null
+     **/
+    get: function(id, callback) {
+        if (!id) return callback("Error: No id supplied");
+        var db = this._getDB();
+        // Create a document for the client in the clients database
+        db.get(id, null, function(err, object) {
+            if (err) return callback(err);
+            return callback(err, object);
+        });     
+    },
+    
+    /**
+      Returns an instance of the db for this helper
+     **/
+    _getDB: function() {
+        return supercomfy(this.server).use(this.db);
+    },
+    
+    /**
+      Returns a generated UUID from couch to use as the id
+     **/
+    _getUUID: function(callback) {
+        var db = supercomfy(config.couchurl);;      
+        db.get('_uuids', null, callback);
+    }
+    
 };
 
--- test/flatpack_spec.js
+++ test/flatpack_spec.js
@@ -1,40 +1,46 @@
 var expect = require('chai').expect,
     flatpack = require('../flatpack'),
-	views = require('../lib/views'),
+    views = require('../lib/views'),
     config = require('config');
 
 describe('defining a flatpack model definition', function() {
     
     it('should define and create a model with a default view', function(done) {
-	
-		flatpack.define(config.couchurl, config.db, 'customer', null, function(err) {
-			if (err) return done(err);
-			views.exists(config.couchurl, config.db, 'customer', null, function(err, exists) {
+    
+        flatpack.define(config.couchurl, config.db, 'customer', null, function(err) {
+            if (err) return done(err);
+            views.exists(config.couchurl, config.db, 'customer', null, function(err, exists) {
                 if (err || !exists) return done(err);
                 return done();
             });
-		});
+        });
     });
 
-	it('should define and create a model with a complex view', function(done) {
-	
-		flatpack.define(config.couchurl, config.db, 'customer', {views: {byName: ['firstName', 'lastName']}}, function(err) {
-			if (err) return done(err);
-			views.exists(config.couchurl, config.db, 'customer', ['firstName', 'lastName'], function(err, exists) {
+    it('should define and create a model with a complex view', function(done) {
+    
+        flatpack.define(config.couchurl, config.db, 'customer', {views: {byName: ['firstName', 'lastName']}}, function(err) {
+            if (err) return done(err);
+            views.exists(config.couchurl, config.db, 'customer', ['firstName', 'lastName'], function(err, exists) {
                 if (err || !exists) return done(err);
                 return done();
             });
-		});
+        });
     });
 
-	it('should be able to save an object using a defined object', function(done) {
-		
-		var customer = {firstName: 'Nathan', lastName: 'Oehlman', company: 'Sidelab'},
-			customerdb = flatpack.use('customer');
-			
-		customerdb.save(customer, function(err) {
-			return done(err);
-		});
-	});
+    it('should be able to save an object using a defined object and can retrieve by id', function(done) {
+        
+        var customer = {firstName: 'Nathan', lastName: 'Oehlman', company: 'Sidelab'},
+            customerdb = flatpack.use('customer');
+            
+        customerdb.save(customer, function(err, id) {
+            if (err) return done(err);
+            customerdb.get(id, function(err, object) {
+                if (err) return done(err);
+                console.log(object);
+                expect(object.firstName).to.equal('Nathan');
+                return done();
+            });
+        });
+    });
     
 });
